62748dfdb18774cc1fd96a81bba9528525d47866
satinder singh <satindersingh472@gmail.com>
2023-06-09 14:36:59 +0530

    basic_Setup_done

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,20 @@
   <v-app>
     <nav-bar></nav-bar>
     <v-main>
-      <router-view />
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import NavBar from './components/navbars/NavBar.vue';
+import NavBar from '@/components/navbars/NavBar.vue'
 export default {
-  components:{NavBar},
-  name: "App",
-  metaInfo:{
-    title: 'Jyoti Plumbing Solutions',
-    titleTemplate: '%s | Jyoti Plumbing Solutions',
-    link:[
-            {
-                rel:'icon',
-                type: 'image/png',
-                href: require("@/assets/logo.png")
-            },
-
-        ]
-  },
-
+  name: 'App',
+components:{
+  NavBar
+},
   data: () => ({
     //
   }),
 };
 </script>
-<style scoped>
-*{
-  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -3,14 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify'
 import router from './router'
-import store from './store'
 
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
