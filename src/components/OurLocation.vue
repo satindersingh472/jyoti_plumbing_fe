08ae6bac9283b90ac611258bff1dbc3a3ea3721e
satinder <satindersingh472@gmail.com>
2023-06-14 12:05:59 -0600

    added_locations

---
+++ src/components/OurLocation.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <v-container class="grid">
+      <v-row>
+        <p class="primary--text text-h4">Locations</p>
+      </v-row>
+      <v-row no-gutters class="mt-3">
+        <p class="pa-2">
+          We have 2 locations to provide quick services in different areas
+        </p>
+      </v-row>
+      <v-row no gutters>
+        <v-col
+          align-self="center"
+          class="grid"
+          cols="12"
+          md="6"
+          v-for="(location, index) in locations"
+          :key="index"
+        >
+          <!-- <v-card-title class="font-weight-regular" >{{ location['name'] }}</v-card-title -->
+          <v-card class="pa-4 grid" elevation="5" height="200px">
+            <v-card-subtitle class="pa-0">{{location['name']}} location Address</v-card-subtitle>
+            <v-card-text class="mt-3 text-body-1">
+              {{ location["street"] }}, {{ location["city"] }},
+              {{ location["state"] }}
+            </v-card-text>
+            <v-card-actions
+              ><v-btn class="primary" :href="location['google_link']"
+                >Get Directions</v-btn
+              ></v-card-actions
+            >
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      locations: [
+        {
+          name: "Devigarh",
+          street: "Bhambuan Road (Opp. Shiv Mandir) ",
+          city: "Devigarh",
+          state: "Punjab",
+          pincode: "147111",
+          google_link: "https://goo.gl/maps/q17TPdftNxCNZELx5",
+        },
+        {
+          name: "Naneola",
+          street: "Near HDFC bank",
+          city: "Naneola (Ambala) ",
+          state: "Haryana",
+          pincode: "144777",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  place-items: center;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <v-container fluid class="fill-height" style="display: grid;place-items:center" >
+    <v-container
+      fluid
+      class="fill-height"
+      style="display: grid; place-items: center"
+    >
       <v-row justify="center">
         <v-col
           class="mt-2 text-center"
@@ -17,31 +21,36 @@
           </p>
         </v-col>
       </v-row>
-     <cards-description ></cards-description>
-
-     <our-brands></our-brands>
+      <cards-description></cards-description>
+      <our-brands></our-brands>
+      <our-location></our-location>
     </v-container>
   </div>
 </template>
 
 <script>
-import CardsDescription from '@/components/CardsDescription.vue';
+import CardsDescription from "@/components/CardsDescription.vue";
 // import PhotoGallery from '@/components/PhotoGallery.vue';
-import OurBrands from '@/components/OurBrands.vue'
+import OurBrands from "@/components/OurBrands.vue";
+import OurLocation from "@/components/OurLocation.vue";
 export default {
-    components:{
-        CardsDescription,
-        // PhotoGallery,
-        OurBrands,
-    },
+  components: {
+    CardsDescription,
+    OurBrands,
+    OurLocation,
+  },
   metaInfo: {
     titleTemplate: "Home - Jyoti Plumbing Solutions",
     meta: [
       {
         name: "description",
         content:
-          "Jyoti Plumbing Solutions provide 10 years free service guarantee for new house projects.",
+          "Jyoti Plumbing Solutions provide 10 years free service guarantee for new house projects. Our services are 100% percent guaranteed for any kind of leaks. ",
       },
+      {
+        name:'keywords',
+        content:'best plumbing services, 100% service guarantee, plumber in devigarh, plumber in naneola, plumber in patiala'
+      }
     ],
   },
   data() {
