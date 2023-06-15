072858adeab8ce48313911abec761a83df5ecc86
satinder <satindersingh472@gmail.com>
2023-06-15 13:53:11 -0600

    adjusted_navbar_justification

--- src/components/navbars/NavBar.vue
+++ src/components/navbars/NavBar.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
     <v-app-bar app elevate-on-scroll>
-      <v-row no-gutters >
-        <v-col cols="4" align-self="center" ><v-img
-        contain
-        src="@/assets/logo.png"
-        width="100px"
-        height="50px"
-        position="center"
-      ></v-img></v-col>
-      <!-- <v-spacer></v-spacer> -->
-      <v-col cols="8" align-self="center" ><desktop-nav-bar v-if="desktop_view === true"></desktop-nav-bar>
-      <mobile-nav-bar v-if="desktop_view === false"></mobile-nav-bar></v-col>
+      <v-row no-gutters>
+        <v-col cols="4" align-self="center"
+          ><v-img
+            contain
+            src="@/assets/logo.png"
+            width="100px"
+            height="50px"
+            position="center"
+          ></v-img
+        ></v-col>
+        <v-col style="justify-items: end;display: grid;" align-self="center"
+          ><desktop-nav-bar v-if="desktop_view === true"></desktop-nav-bar
+          ><mobile-nav-bar v-if="desktop_view === false"></mobile-nav-bar
+        ></v-col>
       </v-row>
-     
     </v-app-bar>
   </div>
 </template>
@@ -27,9 +29,9 @@ export default {
     /*this logic is for the media queries when width is less than 500 then desktop is false 
     and when width is 500 or more desktop view is true and these conditions will change the html on the page*/
     change_querie() {
-      if (document.documentElement.clientWidth < 500) {
+      if (document.documentElement.clientWidth < 900) {
         this.desktop_view = false;
-      } else if (document.documentElement.clientWidth >= 500) {
+      } else if (document.documentElement.clientWidth >= 900) {
         this.desktop_view = true;
       }
     },
@@ -49,7 +51,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.grid{
+.grid {
   display: grid;
   place-items: center;
-}</style>
+}
+</style>
